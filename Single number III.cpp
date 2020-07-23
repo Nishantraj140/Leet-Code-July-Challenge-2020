2e9dddecd8812fb7010ec908c36892cea58655c6
Nishant Raj <rnishant208@gmail.com>
2020-07-23 15:04:44 +0530

    Single Number III(C++)

---
+++ Single number III.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        vector<int>ans;
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto it=m.begin();it!=m.end();it++){
+            if(it->second==1)
+                ans.push_back(it->first);
+        }
+        return ans;
+    }
+};
